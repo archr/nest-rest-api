9506382efc05f2b4cd90c6a2fc8d712ffe672c4b
Antonio <berser.ar@gmail.com>
2017-06-06 16:33:40 -0500

    Allow cars

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
+const cors = require('kcors')
 
 const app = new Koa()
 require('koa-qs')(app)
 
 app.use(logger())
+app.use(cors())
 
 app.use(require('./routes/jobs'))
 app.use(require('./routes/items'))
